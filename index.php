3c2fd56fb599e149c238f64cfa195f345322ef18
chan jun <ckswns879@naver.com>
2023-04-20 15:28:06 +0900

    First commint: php start

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="eg">
+<head>
+	<meta charset="UTF-8">
+	<mate http-equiv="X-UA-Compatible" content="IE=edge">
+	<mate name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php echo "php 에서 실행된 부분"; ?>
+</body>
+</html>
